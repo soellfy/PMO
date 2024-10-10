81cbdd53685685b9442fbdec4cb3f4e3090e82b9
soellfy <95313284+soellfy@users.noreply.github.com>
2024-10-10 14:56:28 +0200

    Add files via upload

---
+++ 02-student-example/TestStudent.java
@@ -0,0 +1,19 @@
+class TestStudent {
+  public static void main(String[] args) {
+      Student student = new Student(Maksym, Lesyuk, 327526, 2023);
+      /*
+       * aggiornare l'invocazione del metodo build con l'aggiunta dei
+       * parametri
+       */
+      student.printStudentInfo();
+      /*
+       * Creare dei nuovi oggetti relativi agli studenti:
+       *
+       * - Nome: Luigi Cognome: Gentile id: 1015, matriculationYear: 2019
+       * - Nome: Simone Cognome: Bianchi id: 1016, matriculationYear: 2018
+       * - Nome: Andrea Cognome: Bracci id: 1017, matriculationYear: 2017
+       *
+       * Stampare a video le informazioni relative a ciascuno studente.
+       */
+  }
+}
