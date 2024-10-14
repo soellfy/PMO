15b40ae96a673d9504ca819450b0bf797d52890f
soellfy <95313284+soellfy@users.noreply.github.com>
2024-10-14 10:16:07 +0200

    Add files via upload

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+package HelloWorld;
+
+/*
+ * Esempio di prima applicazione
+ */
+public class HelloWorld {
+    /*
+     * Main dell'applicazione
+     */
+    public static void main(String[] args) {
+        /*
+         * Stampa "Hello World!" in standard output
+         */
+        System.out.println("Hello, world! " + (50 + 50 * 50));
+        System.out.println("Poop");
+    }
+}
